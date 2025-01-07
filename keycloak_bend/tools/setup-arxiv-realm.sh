9e107ad53b888ba4589088b52835e791e27355ed
Naoyuki Tai <ntai@arxiv.org>
2025-01-06 23:11:19 -0500

    setup of arxiv-db and keycloak are working now. It is a little mind bending to figure this out. You cannot ues localhost from the containers.

--- keycloak_bend/tools/setup-arxiv-realm.sh
+++ keycloak_bend/tools/setup-arxiv-realm.sh
@@ -4,11 +4,11 @@ BEND_DIR=$(dirname "$SCRIPT_DIR")
 
 pip install python-keycloak requests
 
-until curl -s -o /dev/null -w '%{http_code}' http://keycloak:${KC_PORT}/auth/realms/master > /dev/null;
+until curl -s -o /dev/null -w '%{http_code}' $KC_URL/realms/master > /dev/null;
   do
     sleep 5;
   done;
+set -x
+echo python $TOOLS_DIR/setup_arxiv_realm.py --server $KC_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "$LEGACY_AUTH_API_TOKEN"
 
-echo python $TOOLS_DIR/setup_arxiv_realm.py --server $KEYCLOAK_SERVER_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "${LEGACY_AUTH_TOKEN}"
-
-python $TOOLS_DIR/setup_arxiv_realm.py --server $KEYCLOAK_SERVER_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "${LEGACY_AUTH_TOKEN}"
+python $TOOLS_DIR/setup_arxiv_realm.py --server $KC_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "$LEGACY_AUTH_API_TOKEN"
