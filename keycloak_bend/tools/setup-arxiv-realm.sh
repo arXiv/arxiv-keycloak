65b84aaead12e01a9ad915703cd804b1d27c1767
Naoyuki Tai <ntai@arxiv.org>
2025-01-07 13:08:42 -0500

    Keycloak is talking to legacy auth provider.

--- keycloak_bend/tools/setup-arxiv-realm.sh
+++ keycloak_bend/tools/setup-arxiv-realm.sh
@@ -9,6 +9,6 @@ until curl -s -o /dev/null -w '%{http_code}' $KC_URL/realms/master > /dev/null;
     sleep 5;
   done;
 set -x
-echo python $TOOLS_DIR/setup_arxiv_realm.py --server $KC_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "$LEGACY_AUTH_API_TOKEN"
 
-python $TOOLS_DIR/setup_arxiv_realm.py --server $KC_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "$LEGACY_AUTH_API_TOKEN"
+# http://legacy-auth-provider is from docker-compose.yaml
+python $TOOLS_DIR/setup_arxiv_realm.py --server $KC_URL --admin-secret $KC_ADMIN_PASSWORD --arxiv-user-secret "$ARXIV_USER_SECRET" --legacy-auth-token "$LEGACY_AUTH_API_TOKEN" --legacy-auth-uri "$URI"
