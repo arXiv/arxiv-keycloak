2f49699b70b3a2b171881ebdce4e85a5905b8ccd
Naoyuki Tai <ntai@arxiv.org>
2025-01-09 00:46:29 -0500

    Fix build issues from scratch

--- config.sh
+++ config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ACCOUNT=arxiv.1password.com
+
+
 if [ ! -r .env.localdb ] ; then
     PLATFORM=linux/amd64
     GCP_PROJECT=arxiv-development
@@ -39,12 +42,12 @@ if [ ! -r .env.localdb ] ; then
     echo KC_DB_HOST_PRIVATE=auth-db >> .env.localdb
     echo KC_DB_PORT=21502 >> .env.localdb
     echo KC_DB_USER=keycloak >> .env.localdb
-    echo KC_DB_PASS=$(op item get wos2wdt56jx2gjmvb4awlxk3ay --format=json | jq -r '.fields[] | select(.id == "vlf6422dpbnqhne535fpgg4vqm") | .value') >> .env.localdb
-    echo KC_ADMIN_PASSWORD=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "password") | .value') >> .env.localdb
+    echo KC_DB_PASS=$(op item get  wos2wdt56jx2gjmvb4awlxk3ay --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "vlf6422dpbnqhne535fpgg4vqm") | .value') >> .env.localdb
+    echo KC_ADMIN_PASSWORD=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "password") | .value') >> .env.localdb
     echo GCP_PROJECT=$GCP_PROJECT >> .env.localdb
     echo KC_JDBC_CONNECTION="?ssl=false&sslmode=disable" >> .env.localdb
     echo GCP_CRED= >> .env.localdb
-    echo ARXIV_USER_SECRET=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "gxogpm2ztuyfeyvzjrwx4gqogi") | .value') >> .env.localdb
+    echo ARXIV_USER_SECRET=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "gxogpm2ztuyfeyvzjrwx4gqogi") | .value') >> .env.localdb
     # audit logging to GCP subsub
     # keycloak-tapir bridge uses this to update the tapir tables
     echo GCP_EVENT_TOPIC_ID=keycloak-arxiv-events >> .env.localdb
@@ -82,7 +85,7 @@ if [ ! -r .env.localdb ] ; then
     LEGACY_AUTH_PORT=21505
     echo LEGACY_AUTH_PORT=${LEGACY_AUTH_PORT} >> .env.localdb
     # This is the dev-token but for local, use something else
-    # echo LEGACY_AUTH_API_TOKEN=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "rs25xevxhbvy6l2aom7z633rti") | .value') >> .env.localdb
+    # echo LEGACY_AUTH_API_TOKEN=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "rs25xevxhbvy6l2aom7z633rti") | .value') >> .env.localdb
     echo LEGACY_AUTH_API_TOKEN=legacy-api-token >> .env.localdb
     echo LEGACY_AUTH_DOCKER_TAG=gcr.io/$GCP_PROJECT/arxiv-keycloak/legacy-auth-provider >> .env.localdb
     #
@@ -112,16 +115,16 @@ fi
 
 if [ ! -r .env.devdb ] ; then
     echo KC_DOCKER_TAG=gcr.io/$GCP_PROJECT/keycloak >> .env.devdb
-    echo KC_DB_HOST_PUBLIC=$(op item get wos2wdt56jx2gjmvb4awlxk3ay --format=json | jq -r '.fields[] | select(.id == "fnxbox5ugfkr2ol5wtqbk6wkwq") | .value') >> .env.devdb
-    echo KC_DB_HOST_PRIVATE=$(op item get wos2wdt56jx2gjmvb4awlxk3ay --format=json | jq -r '.fields[] | select(.id == "o4idffxy6bns7nihak4q4lo3xe") | .value') >> .env.devdb
+    echo KC_DB_HOST_PUBLIC=$(op item get  wos2wdt56jx2gjmvb4awlxk3ay --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "fnxbox5ugfkr2ol5wtqbk6wkwq") | .value') >> .env.devdb
+    echo KC_DB_HOST_PRIVATE=$(op item get  wos2wdt56jx2gjmvb4awlxk3ay --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "o4idffxy6bns7nihak4q4lo3xe") | .value') >> .env.devdb
     echo KC_DB_USER=keycloak >> .env.devdb
-    echo KC_DB_PASS=$(op item get wos2wdt56jx2gjmvb4awlxk3ay --format=json | jq -r '.fields[] | select(.id == "vlf6422dpbnqhne535fpgg4vqm") | .value') >> .env.devdb
-    echo KC_ADMIN_PASSWORD=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "password") | .value') >> .env.devdb
+    echo KC_DB_PASS=$(op item get  wos2wdt56jx2gjmvb4awlxk3ay --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "vlf6422dpbnqhne535fpgg4vqm") | .value') >> .env.devdb
+    echo KC_ADMIN_PASSWORD=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "password") | .value') >> .env.devdb
     echo GCP_PROJECT=$GCP_PROJECT >> .env.devdb
     echo KC_JDBC_CONNECTION= >> .env.devdb
-    echo GCP_CRED=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "bwh5wxl5lw4yfc3lf53azij4ny") | .value') >> .env.devdb
-    echo ARXIV_USER_SECRET=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "gxogpm2ztuyfeyvzjrwx4gqogi") | .value') >> .env.devdb
-    echo LEGACY_AUTH_API_TOKEN=$(op item get bdmmxlepkfsqy5hfgfunpsli2i --format=json | jq -r '.fields[] | select(.id == "rs25xevxhbvy6l2aom7z633rti") | .value') >> .env.devdb
+    echo GCP_CRED=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "bwh5wxl5lw4yfc3lf53azij4ny") | .value') >> .env.devdb
+    echo ARXIV_USER_SECRET=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "gxogpm2ztuyfeyvzjrwx4gqogi") | .value') >> .env.devdb
+    echo LEGACY_AUTH_API_TOKEN=$(op item get  bdmmxlepkfsqy5hfgfunpsli2i --account arxiv.1password.com --format=json | jq -r '.fields[] | select(.id == "rs25xevxhbvy6l2aom7z633rti") | .value') >> .env.devdb
 fi
 
 if [ ! -r .env ] ; then
