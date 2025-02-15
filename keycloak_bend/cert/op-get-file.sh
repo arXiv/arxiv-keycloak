70735d3777a005bceb74c9b1a5a8078d74169fe9
Naoyuki Tai <ntai@arxiv.org>
2025-02-15 12:00:24 -0500

    Fix the sql file download. Fix the dev db cert download.

--- keycloak_bend/cert/op-get-file.sh
+++ keycloak_bend/cert/op-get-file.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
 ITEM_ID=$1
 shift
 
 OP_FILENAME=$1
 shift
 
-op item get $ITEM_ID --account arxiv.1password.com --format json | jq -r ".files[] | select(.name == \"$OP_FILENAME\") | .content_path" | awk -F / '{ print "op://"$4"/"$6"/"$8 }' | xargs op read
+OUT_FILENAME=$1
+shift
+
+op item get --account arxiv.1password.com $ITEM_ID --format json | jq -r ".files[] | select(.name == \"$OP_FILENAME\") | .content_path" | awk -F / '{ print "op://"$4"/"$6"/"$8 }' | xargs op read --account arxiv.1password.com -o ./$OUT_FILENAME
