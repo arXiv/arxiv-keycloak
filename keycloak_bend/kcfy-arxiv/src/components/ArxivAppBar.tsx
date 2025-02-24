9bb44282c5befad56d8f8c91ac54c4cbe254e883
Naoyuki Tai <ntai@arxiv.org>
2025-02-23 23:43:20 -0500

    In keycloakify, you cannot use the public/static. All of assets need to be under src/ to access it.

--- keycloak_bend/kcfy-arxiv/src/components/ArxivAppBar.tsx
+++ keycloak_bend/kcfy-arxiv/src/components/ArxivAppBar.tsx
@@ -2,6 +2,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import arXivLogo from '../assets/images/arxiv-logo-one-color-white.svg';
 
 
 const ArxivAppBar = () => {
@@ -10,7 +11,7 @@ const ArxivAppBar = () => {
     <AppBar position="sticky" sx={{backgroundColor: '#B31B1B'}}>
         <Container maxWidth="xl">
             <Toolbar disableGutters>
-                <img src="/static/images/arxiv-logo-one-color-white.svg?react" width="85"
+                <img src={arXivLogo} width="85"
                      alt="arXiv Logo" aria-label="arxiv-logo" />
                 <Box sx={{width: 20}}/>
 
