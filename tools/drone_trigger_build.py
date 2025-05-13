7a76fc799def452f3b67023a1841f5d27a192cfa
Norbert Preining <norbert@arxiv.org>
2025-05-13 15:26:39 +0200

    Add support for gcp secret manager, better error reporting, deal with pending etc

--- tools/drone_trigger_build.py
+++ tools/drone_trigger_build.py
@@ -6,11 +6,46 @@ import os
 import time
 
 import requests
+from enum import Enum
 
 DRONE_SERVER = os.environ.get('DRONE_SERVER', 'https://drone.arxiv.org')
-if not os.environ.get('DRONE_TOKEN'):
-    raise OSError('Missing DRONE_TOKEN environment variable')
-DRONE_TOKEN = os.environ['DRONE_TOKEN']
+
+
+def _setup_drone_token() -> str|None:
+    do_local = False
+    drone_token = None
+    try:
+        from google.cloud import secretmanager
+        from google.api_core.exceptions import NotFound
+    except (ImportError, ModuleNotFoundError) as e:
+        do_local = True
+
+    if not do_local:
+        try:
+            client = secretmanager.SecretManagerServiceClient()
+            name = "projects/arxiv-development/secrets/drone_token/versions/latest"
+            response = client.access_secret_version(name=name)
+            drone_token = response.payload.data.decode('UTF-8')
+        except (ImportError, ModuleNotFoundError, NotFound) as e:
+            do_local = True
+
+    if do_local:
+        # If the secret manager is not available, use the environment variable
+        # This is for local development only
+        logging.warning("Google Cloud Secret Manager not available. Using environment variable.")
+        if os.environ.get('DRONE_TOKEN'):
+            drone_token = os.environ['DRONE_TOKEN']
+
+    return drone_token
+
+class DroneBuildStatus(str, Enum):
+    """Drone build pipeline status."""
+
+    success = "success"
+    failure = "failure"
+    timeout = "timeout"
+
+
 
 class DroneBuild:
     """Class to trigger a build on a drone server and wait for it to finish."""
@@ -21,6 +56,7 @@ class DroneBuild:
         self.branch: str = branch
         self.commit: str = commit
         self.nr: int|None = None
+        self.status: DroneBuildStatus|None = None
 
     def _send_request(self, component, method='GET', params=None):
         """Send a request to the Drone API.
@@ -31,6 +67,7 @@ class DroneBuild:
             params (dict): The parameters to send with the request.
         """
         url = f"{DRONE_SERVER}/api/repos/{self.namespace}/{self.repo}/{component}"
+        logging.debug(f"Calling {url} with params {params}")
         headers = {
             'Authorization': f'Bearer {DRONE_TOKEN}',
             'Content-Type': 'application/json'
@@ -62,14 +99,14 @@ class DroneBuild:
         self.nr = response.json()['number']
 
 
-    def is_running(self) -> bool:
+    def current_status(self) -> bool:
         """Check if the build is currently running."""
         if self.nr is None:
             raise ValueError("Build number is not set. Trigger the build first.")
         response = self._send_request(f"builds/{self.nr}", method='GET')
         status = response.json()['status']
         logging.debug(f"Build status: {status}")
-        return status == 'running'
+        return status
 
 
     def wait_until_finished(self, timeout=600) -> None:
@@ -78,14 +115,25 @@ class DroneBuild:
             raise ValueError("Build number is not set. Trigger the build first.")
 
         budget = timeout
-        while self.is_running():
-            logging.debug("Waiting for build to finish...")
+        while True:
             time.sleep(10)
             budget -= 10
             if budget <= 0:
                 # TODO should we try to stop the build?
                 logging.error("Timeout waiting for build to finish.")
-                raise TimeoutError("Timeout waiting for build to finish.")
+                self.status = DroneBuildStatus.timeout
+                return
+            status = self.current_status()
+            if status == 'success':
+                logging.info("Build finished successfully.")
+                self.status = DroneBuildStatus.success
+                return
+            elif status == 'failure' or status == 'error' or status == 'killed':
+                logging.error(f"Build failed, status = {status}.")
+                self.status = DroneBuildStatus.failure
+                return
+            elif status == 'pending':
+                logging.info("Build is pending.")
 
 
 if __name__ == "__main__":
@@ -100,8 +148,18 @@ if __name__ == "__main__":
         loglevel = getattr(logging, args.log.upper(), None)
         logging.basicConfig(level=loglevel)
 
+    DRONE_TOKEN: str|None = _setup_drone_token()
+    if not DRONE_TOKEN:
+        raise OSError('Cannot get DRONE_TOKEN from GCP secret manager or environment variable')
     drone_build = DroneBuild(repo=args.repo, branch=args.branch, commit=args.commit)
     drone_build.trigger()
     drone_build.wait_until_finished(timeout=600)
-    logging.info("Build finished.")
-
+    if drone_build.status == DroneBuildStatus.success:
+        logging.info("Build finished successfully.")
+        exit(0)
+    elif drone_build.status == DroneBuildStatus.failure:
+        logging.error("Build failed.")
+        exit(1)
+    else:
+        logging.error("Build timed out.")
+        exit(2)
