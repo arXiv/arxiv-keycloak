e7bffb1b561bbfcde2285557ac4a16252fcc3520
Naoyuki Tai <ntai@arxiv.org>
2025-08-11 15:31:10 -0400

    Forgot to add 2 important files.

---
+++ account-ui/account-app/src/types/admin-url.ts
@@ -0,0 +1,37 @@
+export const ADMIN_CATEGORIES_URL = '/v1/categories/';
+export const ADMIN_CATEGORY_URL = '/v1/categories/{id}';
+export const ADMIN_CATEGORY_ARCHIVE_SUBJECT_CLASS_URL = "/v1/categories/{archive}/subject-class/{subject_class}"
+
+export const ADMIN_COUNTRIES_URL = '/v1/countries/iso2';
+
+export const ADMIN_DOCUMENT_PAPER_ID_UPL = '/v1/documents/paper_id/{paper_id}'
+export const ADMIN_DOCUMENTS_URL = '/v1/documents/';
+export const ADMIN_DOCUMENTS_ID_URL = '/v1/documents/{id}';
+export const ADMIN_DOCUMENTS_USER_ACTION_URL = '/v1/documents/user-action/{id}/{action}';
+
+export const ADMIN_ENDORSEMENTS_URL = '/v1/endorsements/';
+export const ADMIN_ENDORSEMENT_URL = '/v1/endorsements/{id}';
+export const ADMIN_ENDORSEMENT_ENDORSE_URL = "/v1/endorsements/endorse";
+
+export const ADMIN_ENDORSEMENT_REQUESTS_CODE = "/v1/endorsement_requests/code";
+
+
+export const ADMIN_OWNERSHIP_REQUESTS_URL = '/v1/ownership_requests/';
+export const ADMIN_OWNERSMIP_REQUESTS_ID_URL = '/v1/ownership_requests/{id}';
+
+export const ADMIN_PAPER_OWNERS_URL = '/v1/paper_owners/';
+export const ADMIN_PAPER_OWNERS_AUTHORSHIP_ACTION_URL = '/v1/paper_owners/authorship/{action}';
+export const ADMIN_PAPER_OWNERS_ID_URL = '/v1/paper_owners/{id}';
+export const ADMIN_PAPER_OWNERS_AUTHORIZE_URL ='/v1/paper_owners/authorize/';
+
+export const ADMIN_PAPER_PW_ID_URL = '/v1/paper_pw/{id}';
+
+export const ADMIN_PUBLIC_USER_URL = '/v1/public-users/{user_id}'
+
+export const ADMIN_SUBMISSIONS_METADATA_STATUS_LIST_URL = '/v1/submissions/metadata/status-list';
+export const ADMIN_SUBMISSIONS_URL = '/v1/submissions/';
+export const ADMIN_SUBMISSIONS_ID_URL = '/v1/submissions/{id}';
+
+export const ADMIN_TAXONOMY_GROUPS_URL = '/v1/taxonomy/groups/{group_id}';
+
+export const ADMIN_USER_ID_DOCUMENT_SUMMARY_URL = '/v1/users/{user_id}/document-summary';
