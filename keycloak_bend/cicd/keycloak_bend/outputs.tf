fe901bcd36c8b43794bd7b32e13c24e4db4830d8
Mike Greco <mike@arxiv.org>
2025-09-10 21:44:07 -0400

    updates

--- keycloak_bend/cicd/keycloak_bend/outputs.tf
+++ keycloak_bend/cicd/keycloak_bend/outputs.tf
@@ -1,3 +1,13 @@
 output "keycloak_backend_service_id" {
   value = google_compute_region_backend_service.keycloak_backend.id
 }
+
+output "keycloak_cloud_run_url" {
+  description = "The direct URL to the Keycloak Cloud Run service."
+  value       = google_cloud_run_service.keycloak.status[0].url
+}
+
+output "keycloak_lb_url" {
+  description = "The URL to the Keycloak admin console through the load balancer."
+  value       = "http://${data.terraform_remote_state.env.outputs.load_balancer_ip_address}/auth/admin/"
+}
