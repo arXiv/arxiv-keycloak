bc802e4263950b0688422ab94b50b4b03e1b8a93
Mike Greco <mike@arxiv.org>
2025-11-04 22:09:51 -0500

    updates

--- terraform/keycloak-service/main.tf
+++ terraform/keycloak-service/main.tf
@@ -187,7 +187,7 @@ resource "google_cloud_run_service" "keycloak" {
           name = "KC_DB_PASS"
           value_from {
             secret_key_ref {
-              name    = "auth-db-password"
+              name    = "authdb-db-password"
               key     = "latest"
             }
           }
