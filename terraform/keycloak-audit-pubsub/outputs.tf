44177cb11029f24b87539f362dbdf5cd04d4b8c3
Naoyuki Tai <ntai@arxiv.org>
2025-11-22 11:00:44 -0500

    Add the pubsub subscriber and SA to access it.

--- terraform/keycloak-audit-pubsub/outputs.tf
+++ terraform/keycloak-audit-pubsub/outputs.tf
@@ -15,3 +15,8 @@ output "service_account_email" {
   value       = google_service_account.keycloak_pubsub_sa.email
   description = "Email of the Pub/Sub publisher service account"
 }
+
+output "subscriber_credentials_secret_name" {
+  value       = google_secret_manager_secret.keycloak_subscriber_credentials.secret_id
+  description = "Secret Manager secret name for subscriber service account credentials"
+}
